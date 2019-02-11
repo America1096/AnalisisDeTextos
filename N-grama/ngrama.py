7931900ac1f3ff9deb6d4a00238d9a7ef3b736c0
lissetla <lizzeth.shavez@gmail.com>
2019-02-10 20:50:03 -0600

    Tarea1

---
+++ N-grama/ngrama.py
@@ -0,0 +1,60 @@
+import operator
+
+def Ngrama(n, palabra, archivo):
+	for x in range(len(palabra)-(n-1)):
+		archivo.write(palabra[x:x+n]+"\n")	#Manda a un archivo la palabra en ngramas
+
+def acomodarNgrama(texto, archivo):
+	palabra = texto.split() #Divide el texto en palabras
+	n = int(input("\nIngrese n: "))
+	for x in range(len(palabra)):
+		Ngrama(n, palabra[x], archivo)
+	archivo.seek(0)	#Coloca el apuntador de un archivo en la posicion 0
+	contador = archivo.read().split() #Genera una lista de los ngramas con el archivo antes creado
+	dic = {}
+	for w in contador:
+		dic.update({w : contador.count(w)}) #Se agrega en un diccionario el ngrama y se actualiza el numero de apariciones
+	l = sorted(dic.items(), key=operator.itemgetter(1)) #Se acomoda de menor al mayor numero de apariciones en una lista
+	l.reverse() #Se invierte la lista para que sea de mayor a menor el numero de apariciones
+	return (l)
+
+def mostrarNgramas(ngramas):
+	num = int(input("\nCuantos ngramas desea ver? "))
+	if num > len(ngramas):	print(ngramas) #Si el numero de ngramas que se desea ver es mayor al numero de ngramas se muestran todos
+	else:
+		for x in range(num): #Se muestra el numero de ngramas indicados
+			print(ngramas[x]) 
+	
+def main():
+	arch = open('ngramas.txt','w+')
+	opc = input("\nDesea ingresar texto?\n1)Desde teclado\n2)Desde archivo\n3)Archivos dados\n$$")
+	if opc == "1":
+		texto = input("\nIngrese texto:\n$$")
+		mostrarNgramas(acomodarNgrama(texto, arch))
+	elif opc == "2":
+		doc = input("\nIngrese nombre del archivo(Ej:archivo.txt):\n$$")
+		d = open(doc, 'r')
+		texto = d.read()	
+		mostrarNgramas(acomodarNgrama(texto, arch))
+		d.close()
+	elif opc == "3":
+		esp = open('esp.txt', 'r')
+		ing = open('ing.txt', 'r')
+		frances = open('frances.txt', 'r')
+		texto1 = esp.read()	
+		texto2 = ing.read()	
+		texto3 = frances.read()	
+		print("\n\n\tTexto en español")
+		mostrarNgramas(acomodarNgrama(texto1, arch))
+		print("\n\n\tTexto en ingles")
+		mostrarNgramas(acomodarNgrama(texto2, arch))
+		print("\n\n\tTexto en frances")
+		mostrarNgramas(acomodarNgrama(texto3, arch))
+		esp.close()
+		ing.close()
+		frances.close()
+	else:
+		main()
+	arch.close()
+
+main()
